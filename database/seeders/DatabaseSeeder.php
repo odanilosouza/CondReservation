93337a6ebae6a799008590e860c99e322faaf9fa
Danilo Scott <dev.danilosouza@gmail.com>
2022-10-18 15:46:23 -0300

    Alteracao da seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,30 +40,27 @@ class DatabaseSeeder extends Seeder
         //Areas comuns do condomínio
 
         DB::table('areas')->insert([
-            'allowed' => '1',
             'title' => 'Academia',
-            'cover' => 'gym.jpg',
-            'days' => 1, 2, 4, 5,
+            'covers' => 'gym.jpg',
+            'days' => '1, 2, 4, 5,',
             'start_time' => '06:00:00',
             'end_time' => '22:00:00',
 
         ]);
 
         DB::table('areas')->insert([
-            'allowed' => '1',
             'title' => 'Piscina',
-            'cover' => 'pool.jpg',
-            'days' => 1, 2, 3, 4, 5,
+            'covers' => 'pool.jpg',
+            'days' => '1, 2, 3, 4, 5,',
             'start_time' => '07:00:00',
             'end_time' => '23:00:00',
 
         ]);
 
         DB::table('areas')->insert([
-            'allowed' => '1',
-            'title' => 'Academia',
-            'cover' => 'barbecue.jpg',
-            'days' => 1, 2, 4, 5,
+            'title' => 'Churrasqueira',
+            'covers' => 'barbecue.jpg',
+            'days' => ' 1, 2, 4, 5,',
             'start_time' => '06:00:00',
             'end_time' => '22:00:00',
 
@@ -72,14 +69,14 @@ class DatabaseSeeder extends Seeder
         DB::table('walls')->insert([
             'title' => 'Titulo de aviso de Teste',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            'datacreated' => '2022-10-16 15:00:00',
+            'datecreated' => '2022-10-16 15:00:00',
 
         ]);
 
         DB::table('walls')->insert([
             'title' => 'Alerta geral para todos',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            'datacreated' => '2022-10-16 18:00:00',
+            'datecreated' => '2022-10-16 18:00:00',
 
         ]);
 
